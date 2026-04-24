88aa0d324c1b36bf5e7aeeedba9bbf653be5b3b9
shaunte99 <ksmatsetela99@gmail.com>
2026-04-24 09:49:12 +0200

    Add files via upload

---
+++ scripts/home.js
@@ -0,0 +1,13 @@
+setInterval(()=>{
+let star=document.createElement("div");
+star.style.position="absolute";
+star.style.width="3px";
+star.style.height="3px";
+star.style.background="white";
+star.style.top=Math.random()*window.innerHeight+"px";
+star.style.left=Math.random()*window.innerWidth+"px";
+star.style.opacity=Math.random();
+star.style.borderRadius="50%";
+document.body.appendChild(star);
+setTimeout(()=>{ star.remove(); },2000);
+},140);
